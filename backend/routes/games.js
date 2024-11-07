af5eb42f9742044a36b1c98423cd9eefe4233c63
Sofia Martinez <sofiacaro1284@gmail.com>
2024-11-06 21:31:30 -0500

    updated game routes with controller functions

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,41 +1,24 @@
 const express = require('express')
-//const = {} = require('controller')
+const {
+    getHome,
+    startGame,
+    playHit,
+    playStand
+} = require('../controllers/gameController')
 
 const router = express.Router()
 
 //Get the home page
-router.get('/', (req, res) => {
-    //display home page with start button
-    res.json({mssg: 'Home page'})
-})
+router.get('/', getHome)
 
 //Create/start a new game
-router.post('/start', (req, res) => {
-    //Shuffle deck + deal cards to player and dealer
-    res.json({mssg: 'start  a game'})
-})
+router.post('/start', startGame)
 
 //Update game to simulate player hit
-router.post('/hit', (req, res) => {
-    //Get specified game
-    const {id} = req.params
-    //check for valid id, assign a game constant(findById)
-    //if game not found, return error
-    //add new card to player's hand, update game status, update if player can hit again
-    //return ok status with: updated player hand
-    res.json({mssg: 'play out a hit'})
-})
+router.post('/hit', playHit)
 
 //Update game to simulate player stand
-router.post('/stand', (req,res) => {
-    //Get specified game
-    const {id} = req.params
-    //check for valid id, assign a game constant(findById)
-    //if game not found, return error
-    //play out dealer's turn, determine game winner
-    //update game status, return both hands and the winner
-    res.json({mssg: 'play out a stand'})
-})
+router.post('/stand', playStand)
 
 //Export the router
 module.exports = router
