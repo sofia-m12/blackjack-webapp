dbd7fdef4ee425157710cc98fb1c7b9bfdf3ffa1
Sofia Martinez <sofiacaro1284@gmail.com>
2024-11-10 17:38:28 -0500

    connected server to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-const express = require('express')
 require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const gameRoutes = require('./routes/games.js')
 
 //Middleware: will log any json properties that are written in a route
 app.use(express.json());
 app.use('/api/game', gameRoutes)
-//Listener
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port 5500!')
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    //Listener
+    app.listen(process.env.PORT, () => {
+    console.log('Connected to DB and Listening on port 5500!')
+    })
 })
+.catch((error) => {
+    console.log(error)
+})
+
